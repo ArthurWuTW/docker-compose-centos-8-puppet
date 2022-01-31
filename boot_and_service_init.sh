af01a4fa2d6918c962ab9a3da92deeab1eec869e
ArthurWuTw <lion811004@gmail.com>
2022-01-31 12:23:47 +0800

    init database

--- boot_and_service_init.sh
+++ boot_and_service_init.sh
@@ -1,6 +1,15 @@
 docker-compose up --build --detach
 
-docker-compose exec puppet-master /bin/sh -c "service puppetserver start"
+# Puppet Master
+docker-compose exec puppet-master /bin/sh -c "systemctl enable --now puppetserver"
 
+# Postgres Db
 docker-compose exec postgres-db /bin/sh -c "postgresql-setup initdb"
+docker-compose exec postgres-db /root/init-postgres-config
 docker-compose exec postgres-db /bin/sh -c "systemctl enable --now postgresql"
+# postgres db self test
+docker-compose exec postgres-db /bin/sh -c "sudo -u postgres psql -c 'SELECT version();'"
+# postgres db init user and database
+docker-compose exec postgres-db /root/init-postgres-user
+# set time zone
+docker-compose exec postgres-db /bin/sh -c "timedatectl set-timezone Asia/Taipei"
