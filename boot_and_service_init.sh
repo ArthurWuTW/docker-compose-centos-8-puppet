7ab145ef7f368ae36c244c3cbb6a7ae0f128e71a
ArthurWuTw <lion811004@gmail.com>
2022-02-03 14:05:49 +0800

    rename dockerfile name and add more slaves

--- boot_and_service_init.sh
+++ boot_and_service_init.sh
@@ -5,4 +5,11 @@ docker-compose exec postgres-db /root/init-postgres-config
 docker-compose exec postgres-db /root/init-postgres-user
 
 # Puppet server & Puppet db
-docker-compose exec puppet-db /root/start-puppetserver-puppetdb
+docker-compose exec puppet-master /root/start-puppetserver-puppetdb
+
+# Slave
+docker-compose exec puppet-slave1 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
+docker-compose exec puppet-slave2 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
+docker-compose exec puppet-slave3 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
+docker-compose exec puppet-slave4 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
+docker-compose exec puppet-slave5 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
