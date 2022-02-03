d040594f520ffe883519c0003091721c56642181
ArthurWuTw <lion811004@gmail.com>
2022-02-03 22:21:43 +0800

    Use puppet7 on server and agent

--- boot_and_service_init.sh
+++ boot_and_service_init.sh
@@ -13,3 +13,6 @@ docker-compose exec puppet-slave2 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -
 docker-compose exec puppet-slave3 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
 docker-compose exec puppet-slave4 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
 docker-compose exec puppet-slave5 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
+docker-compose exec puppet-slave6 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
+docker-compose exec puppet-slave7 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
+docker-compose exec puppet-slave8 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
