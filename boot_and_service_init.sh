f9151fd92b2dce5dcd5e6afb724be5c455e2039a
ArthurWuTW <36653598+ArthurWuTW@users.noreply.github.com>
2023-11-21 11:18:12 -0500

    Update boot_and_service_init.sh
    Fix start postgres error

--- boot_and_service_init.sh
+++ boot_and_service_init.sh
@@ -1,18 +1,21 @@
 docker-compose up --build --detach
 
 # Postgres Db
+sleep 10
 docker-compose exec postgres-db /root/init-postgres-config
+sleep 10
 docker-compose exec postgres-db /root/init-postgres-user
 
 # Puppet server & Puppet db
+sleep 10
 docker-compose exec puppet-master /root/start-puppetserver-puppetdb
 
 # Slave
 docker-compose exec puppet-slave1 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
-docker-compose exec puppet-slave2 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
-docker-compose exec puppet-slave3 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
-docker-compose exec puppet-slave4 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
-docker-compose exec puppet-slave5 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
-docker-compose exec puppet-slave6 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
-docker-compose exec puppet-slave7 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
-docker-compose exec puppet-slave8 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
+#docker-compose exec puppet-slave2 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
+#docker-compose exec puppet-slave3 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
+#docker-compose exec puppet-slave4 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
+#docker-compose exec puppet-slave5 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
+#docker-compose exec puppet-slave6 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
+#docker-compose exec puppet-slave7 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
+#docker-compose exec puppet-slave8 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
