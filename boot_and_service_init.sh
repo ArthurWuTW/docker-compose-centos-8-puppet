0e195dfdd6feb7144fd58d9178376d51722ffed1
ArthurWuTW <lion811004@gmail.com>
2023-12-05 11:08:18 -0500

    add puppet lib and mount puppet code

--- boot_and_service_init.sh
+++ boot_and_service_init.sh
@@ -10,6 +10,9 @@ docker-compose exec postgres-db /root/init-postgres-user
 sleep 10
 docker-compose exec puppet-master /root/start-puppetserver-puppetdb
 
+sleep 10
+docker-compose exec puppet-master /bin/sh -c "/opt/puppetlabs/bin/puppet module install puppetlabs-stdlib"
+
 # Slave
 docker-compose exec puppet-slave1 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
 #docker-compose exec puppet-slave2 /bin/sh -c "/opt/puppetlabs/bin/puppet agent -t"
